649c7eacf52052d8defcb5d710ef0d93b1575228
neeraj <nsxylush@gmail.com>
2022-11-13 00:20:00 +0530

    added custom fonts

--- app/src/main/java/com/njsh/reelssaver/ui/pages/PageFacebookVideo.kt
+++ app/src/main/java/com/njsh/reelssaver/ui/pages/PageFacebookVideo.kt
@@ -14,8 +14,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.njsh.reelssaver.App
 import com.njsh.reelssaver.R
@@ -28,6 +30,7 @@ import com.njsh.reelssaver.ui.components.CircularProgressBar
 import com.njsh.reelssaver.ui.components.InputPasteAndGet
 import com.njsh.reelssaver.ui.components.NativeAdView
 import com.njsh.reelssaver.ui.components.TopAppbar
+import com.njsh.reelssaver.ui.theme.AppTheme
 import com.njsh.reelssaver.util.checkStoragePermission
 import com.njsh.reelssaver.util.storagePermission
 import kotlinx.coroutines.launch
@@ -137,5 +140,14 @@ class PageFacebookVideo(private val navController: NavController) : Page("Facebo
                 .fillMaxWidth()
                 .heightIn(max = 360.dp)
         )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewFacebookVideoPage() {
+    val page = PageFacebookVideo(rememberNavController())
+    AppTheme {
+        page.drawContent()
     }
 }
