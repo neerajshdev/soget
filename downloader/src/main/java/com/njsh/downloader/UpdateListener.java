6b6bdf14cb175ed8ed51969d711fd66776f5902f
Neeraj Sharma <nsxylush@gmail.com>
2023-12-27 21:20:34 +0530

    Download module old implementation erased

--- downloader/src/main/java/com/njsh/downloader/UpdateListener.java
+++
@@ -1,7 +0,0 @@
-package com.gd.downloader;
-
-public class UpdateListener {
-    public void onUpdateSpeed(int bytesPerSec) {}
-    public void onUpdateProgress(long complete, long total) {}
-    public void onDownloadFished() {}
-}
